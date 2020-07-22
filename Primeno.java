fff6ffc2820ce0b07fc4e255bc774ea1b413ad40
Preeti Singh <54322860+Preeti-gudiya@users.noreply.github.com>
2020-07-22 11:25:19 +0530

    Add files via upload

---
+++ Primeno.java
@@ -0,0 +1,32 @@
+class Primeno
+{
+static void checkprime(int n)
+{
+int i,m=0,flag=0;
+m=m/2;
+if(n==0||n==1)
+{
+System.out.println(n+"is not prime number");
+}
+else
+{
+for(i=2;i<m;i++)
+if(n%i==0)
+{
+System.out.println(n+"is not a prime number");
+flag=1;
+}
+}
+if(flag==0)
+{
+System.out.println(n+"is prime number");
+}
+}
+public static void main(String args[])
+{
+	checkprime(1);
+	checkprime(3);
+	checkprime(17);
+	checkprime(20);
+}
+}
